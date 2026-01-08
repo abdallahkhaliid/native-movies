f62cfe22eeb515e1c198ec7ded07517ce4d6f4ac
abdallahkhaliid <abdallahsh404@gmail.com>
2026-01-08 16:52:02 +0200

    chore: set app config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
-
-module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
+module.exports = {
+  extends: ["expo", "plugin:import/typescript"],
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project: "./tsconfig.json",
+      },
+    },
   },
-]);
+};
